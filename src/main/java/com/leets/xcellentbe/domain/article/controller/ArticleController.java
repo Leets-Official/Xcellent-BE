f7420adfbe389f2e6d9812107258b08e91516750
Dongyun Kim <djskaldrjfj@naver.com>
2024-11-14 02:05:00 +0900

    refactor: #36 api 오타 수정

--- src/main/java/com/leets/xcellentbe/domain/article/controller/ArticleController.java
+++ src/main/java/com/leets/xcellentbe/domain/article/controller/ArticleController.java
@@ -89,7 +89,7 @@ public class ArticleController {
 	}
 
 	//메인 페이지 게시글 조회
-	@GetMapping("/articles")
+	@GetMapping
 	@Operation(summary = "게시글 목록 조회(스크롤)", description = "페이징을 적용하여 게시글 목록을 조회합니다.")
 	public ResponseEntity<GlobalResponseDto<Page<ArticleResponseDto>>> getArticles(
 		HttpServletRequest request,
